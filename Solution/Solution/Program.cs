ad2b452ba7ec5f9efe9d9c917f089b10c13121d6
nisslyak <nissopodolyak@gmail.com>
2024-06-05 06:38:17 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.ComponentModel.Design;
+
+class Result
+{
+    public static int birthday(List<int> s, int d, int m)
+    {
+        int ways = 0;
+        for (int i = 0; i <= s.Count()-m; i++)
+        {
+            int sum = 0;
+            for (int j = i; j < i + m; j++)
+            {
+                sum += s[j];
+            }
+            if (sum == d)
+            {
+                ways++;
+            }
+        }
+        return ways;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
